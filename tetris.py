41982eda51967e18d9304cfb0c17ae3c01d0dac9
Harry Hauer <geekcat@protonmail.com>
2022-01-26 16:58:16 +0100

    its now oriented object (and collisons are made)

--- tetris.py
+++ tetris.py
@@ -1,75 +1,62 @@
 from microbit import *
 import random
 
+
 def selectionner_barre(liste_barre):
     result = random.choice(liste_barre)
     return result
 
-def descente_barre(piece):
-    """va chercher la hauteur de la barre,
-    puis retourne de combien elle doit descendre"""
-    if piece == barre:
-        result = 1
-    elif piece == barre_s or piece == barre_z:
-        result = 2
-    elif piece == carre or piece == barre_t:
-        result = 3
-    return result
 
-# Définition des différentes barres
-barre_s = Image("09000:"
-                "99000:"
-                "90000:"
-                "00000:"
-                "00000")
+class barre:
+    """Une barre qui a sa position x et y"""
 
-barre_z = Image("90000:"
-                "99000:"
-                "09000:"
-                "00000:"
-                "00000")
+    # Définition des différentes barres
+    barre_s = Image("99000:" "00000:" "00000:" "00000:" "00000")
+    barre_z = Image("09000:" "99000:" "00000:" "00000:" "00000")
+    barre = Image("90000:" "99000:" "00000:" "00000:" "00000")
+    carre = Image("99000:" "99000:" "00000:" "00000:" "00000")
+    barre_t = Image("9000:" "90000:" "90000:" "00000:" "00000")
+    barres = [barre_s, barre_z, barre, barre_t, carre]
 
-barre = Image("90000:"
-              "90000:"
-              "90000:"
-              "90000:"
-              "00000")
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.forme_barre = selectionner_barre(barre.barres)
 
-carre = Image("99000:"
-              "99000:"
-              "00000:"
-              "00000:"
-              "00000")
+    def peutDescendre(self):
+        """va chercher la hauteur de la barre,
+        puis retourne de combien elle doit descendre"""
+        if display.get_pixel(2, self.y) == 0 and self.x < 3:
+            self.x += 1
+            self.afficherBarre(self.x, self.y)
+            return True
 
-barre_t = Image("09000:"
-                "99900:"
-                "00000:"
-                "00000:"
-                "00000")
+    def allerADroite(self):
+        self.y += 1
+        print(self.y)
+        self.afficherBarre(self.x, self.y)
+
+    def allerAGauche(self):
+        self.y -= 1
+        print(self.y)
+        self.afficherBarre(self.x, self.y)
+
+    def afficherBarre(self, pos_x=0, pos_y=0):
+        display.show(self.forme_barre.shift_down(pos_x).shift_right(pos_y))
 
 
-barres = [barre_s, barre_z, barre, barre_t, carre]
-barre_selectionnee = selectionner_barre(barres)
-descente = descente_barre(barre_selectionnee)
+barre_courante = barre()
 barres_posees = []
 
-for i in range(0, descente):
-    display.show(barre_selectionnee.shift_down(i+1))
-    sleep(300)
+barres_posees.append(barre_courante)
+barre_courante.afficherBarre()
 
-barres_posees.append(barre_selectionnee.shift_down(descente))
-print(barres_posees)
+while barre_courante.peutDescendre():
 
-clic_bouton_a = 0
-clic_bouton_b = 0
-
-while True:
     if button_b.was_pressed():
-        clic_bouton_a += 1
-        display.show(barres_posees[-1].shift_right(clic_bouton_a))
-        print(clic_bouton_a)
+        barre_courante.allerADroite()
 
     if button_a.was_pressed():
-        clic_bouton_a -= 1
-        display.show(barres_posees[-1].shift_right(clic_bouton_a))
-        print(clic_bouton_a)
+        barre_courante.allerAGauche()
+
+    sleep(300)
